bfacc45896f7dc66d08a80fb62fe0f7e73bfd854
Jose Moreno <jose@G4N.fritz.box>
2021-09-29 15:41:46 +0200

    first real commit

--- src/Form/ChartControl/ChartControlFormType.php
+++ src/Form/ChartControl/ChartControlFormType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Date;
 
-class ChartControlFormType extends AbstractType
+class CfhartControlFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
---
+++ src/Form/Ticket/TicketFormType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ticket;
+use phpDocumentor\Reflection\Types\Boolean;
+use phpDocumentor\Reflection\Types\Integer;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
+
+class TicketFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('status', ChoiceType::class,[
+                'label'         => 'select the status',
+                'choices'       => [
+                // TO DECIDE
+                ],
+                'required' => true,
+                'placeholder'   => 'please Choose ...'
+            ])
+            ->add('begin', DateType::class,[
+                'label'         => 'Begin',
+                'help'          => '[begin]',
+                'label_html'    => true,
+                'widget'        => 'single_text',
+                'input'         => 'datetime',
+                'empty_data'    => new \DateTime('now'),
+                'required'      =>true,
+            ])
+            ->add('end', DateType::class,[
+                'label'         => 'End',
+                'help'          => '[end]',
+                'label_html'    => true,
+                'widget'        => 'single_text',
+                'input'         => 'datetime',
+                'empty_data'    => new \DateTime('now'),
+            ])
+            ->add('ticketActivity',DateType::class,[
+
+            ])
+            ->add('PR', Boolean::class,[
+
+            ])
+            ->add('PA', Boolean::class,[
+
+            ])
+            ->add('yield', Boolean::class,[
+
+            ])
+            ->add('freeText', TextType::class,[
+
+            ])
+            ->add('description', TextType::class,[
+
+            ])
+            ->add('systemStatus', ChoiceType::class,[
+                'label'         => 'select the status of the system',
+                'choices'       => [
+                //TO DECIDE
+                ],
+                'required' => true,
+                'placeholder'   => 'please Choose ...'
+            ])
+            ->add('priority', ChoiceType::class,[
+                'label'         => 'select the priority',
+                'choices'       => [
+                // TO DECIDE
+                ],
+                'required' => true,
+                'placeholder'   => 'please Choose ...'
+            ])
+            ->add('answer', TextType::class,[
+
+            ])
+            ->add('anlage', EntityType::class,[
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ticket::class,
+        ]);
+    }
+}
