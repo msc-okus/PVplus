5b091b25705b79a399dd89f3b4b16ff88680b5b2
Matthias Reinhardt <mre@reinhardt-online.de>
2021-10-19 15:00:45 +0200

    Stand 18.10.2021

---
+++ migrations/Version20211018114357.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211018114357 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE anlage ADD threshold1_pa VARCHAR(20) DEFAULT NULL, CHANGE min_irradiation_availability threshold2_pa VARCHAR(20) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE anlage ADD min_irradiation_availability VARCHAR(20) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`, DROP threshold1_pa, DROP threshold2_pa');
+    }
+}
