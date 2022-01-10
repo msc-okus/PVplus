dad2877aa3af3b8beddbea6ef0ad9351430688ef
Matthias Reinhardt <mre@reinhardt-online.de>
2022-01-10 17:00:42 +0100

    install encore and webpack

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
