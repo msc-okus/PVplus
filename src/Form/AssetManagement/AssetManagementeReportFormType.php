72c9869c81a5e2e8b109507904f67ba2eb4730cb
JoseMorenoMartin <jm@green4net.com>
2022-01-31 14:04:37 +0100

    form done

--- src/Form/AssetManagement/AssetManagementeReportFormType.php
+++ src/Form/AssetManagement/AssetManagementeReportFormType.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\AssetManagement;
 
+use App\Form\Type\SwitchType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +14,40 @@ class AssetManagementeReportFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('Production',SwitchType::class)
+            ->add('ProductionPos', ChoiceType::class,[
+                'expanded' => false,
+                'multiple' => true,
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                ],
+            ])
+            ->add('Availability',SwitchType::class)
+            ->add('AvailabilityPos', ChoiceType::class,[
+                'expanded' => false,
+                'multiple' => true,
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                ],
+            ])
+            ->add('Economics',SwitchType::class)
+            ->add('EconomicsPos', ChoiceType::class,[
+                'expanded' => false,
+                'multiple' => true,
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'     => 'submit',
+                'attr'      => ['class' => 'primary save'],
+            ])
         ;
     }
 
