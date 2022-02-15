a9a68e527801faaf28ea95716c7a69eb662a6502
JoseMorenoMartin <jm@green4net.com>
2022-02-15 14:37:40 +0100

    all done

--- src/Service/AssetManagementService.php
+++ src/Service/AssetManagementService.php
@@ -383,8 +383,7 @@ class AssetManagementService
         //fuer die Tabelle
 
         #Forecast / degradation
-        $degradation = 4.98;
-
+        $degradation = $anlage->getDegradationForecast()!== 0.0 ? $anlage->getDegradationForecast() : 4.98;
         //Cumulative Forecast
         $kumsum[0] = $powerEvu[0];
         for ($i = 0; $i < 12; $i++) {
