34f15098aba454d2b473c44202fc9167313de2ee
unknown <jose.moreno063@alu.uhu.es>
2022-06-27 12:46:13 +0200

    I got to recognice the checked checkboxes via method in the controller, just need to make a JSON out of that and add it to the request. TARGET WORKING

---
+++ assets/controllers/request_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from '@hotwired/stimulus';
+import $ from 'jquery';
+
+export default class extends Controller {
+
+    static targets = ['box'];
+
+    connect() {
+        // const button = $(this.joinTargets);
+        //const button = this.element.children.namedItem('join');
+        /*
+        var array = [];
+        //const checkboxes = this.element.querySelectorAll('input[type=checkbox]:checked');
+        var checkboxes = this.element;
+
+        const jsonString = JSON.stringify(array);
+        console.log(jsonString);
+
+        $(button).on('click', () => {
+            this.element.innerHTML = checkboxes.length.toString();
+        });
+        */
+        const checkboxes = $(this.boxTargets);
+        console.log(checkboxes);
+        for (var i = 0; i < checkboxes.length; i++) {
+            var checkbox = checkboxes[i];
+            console.log(checkbox.checked);
+
+
+        }
+        /*
+        $(checkboxes).addEventListener('change', () => {
+           this.element.classList.add('checked');
+        });
+*/
+    }
+
+    submit() {
+
+    }
+    check(){
+        event.currentTarget.classList.add('checked');
+        console.log(event.currentTarget.classList);
+    }
+
+}
