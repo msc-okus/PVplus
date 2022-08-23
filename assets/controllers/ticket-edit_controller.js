51c60ed8762a692733e8d41f67a7ea9ff50721b6
Matthias <77692994+mr-g4n@users.noreply.github.com>
2022-08-23 11:14:37 +0200

    use foundatiopn tooltip for user edit form

--- assets/controllers/ticket-edit_controller.js
+++ assets/controllers/ticket-edit_controller.js
@@ -1,9 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
 import { useDispatch } from 'stimulus-use';
 import { Reveal } from 'foundation-sites';
+import { Foundation } from 'foundation-sites';
 import $ from 'jquery';
-
-
 
 export default class extends Controller {
     static targets = ['modal', 'modalBody', 'splitModal', 'splitForm'];
