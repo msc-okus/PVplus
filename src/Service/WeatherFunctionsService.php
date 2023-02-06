47817ded726155bc7a7fcff432b6498115765146
Matthias <77692994+mr-g4n@users.noreply.github.com>
2023-02-06 13:34:06 +0100

    new Version (2.2.14)

--- src/Service/WeatherFunctionsService.php
+++ src/Service/WeatherFunctionsService.php
@@ -181,26 +181,26 @@ class WeatherFunctionsService
         $tag++; // Tag um eins erhöhen, da Formel annimmt das der erste Tag im Jahr = 1 ist und nicht 0 wie format('z') zurück gibt
         $stunde = (integer)$stamp->format('G');
 
-        dump("Tag: $tag | Stunde: $stunde");
+        #dump("Tag: $tag | Stunde: $stunde");
         $moz            = (($laenge - $bezugsmeridian) / 15) + $stunde;
         $lo             = deg2rad(279.3 + 0.9856 * $tag);
         $zgl            = 0.1644 * SIN(2 * ($lo + deg2rad(1.92) * SIN($lo + deg2rad(77.3)))) - 0.1277 * SIN($lo + deg2rad(77.3));
         $woz            = $moz + rad2deg($zgl) / 60;
         $stdWink        = deg2rad(15 * ($woz - 12));
         $deklination    = deg2rad((-23.45) * COS ((2 * PI() / 365.25) * ( $tag + 10 )));
-        dump("Deklination (rad): $deklination");
+        #dump("Deklination (rad): $deklination");
         $sonnenhoehe    = ASIN(SIN($deklination)*SIN(deg2rad($breite))+COS($deklination)*COS(deg2rad($breite))*COS($stdWink));
         $atheta         = ASIN((-(COS($deklination)*SIN($stdWink)))/COS($sonnenhoehe));
         $azimuth        = 180 - rad2deg($atheta);
         $zenitwinkel    = 90 - rad2deg($sonnenhoehe);
         $aoi            = 1 / COS(COS(deg2rad($zenitwinkel))*COS(deg2rad($neigungModul))+SIN(deg2rad($zenitwinkel))*SIN(deg2rad($neigungModul))*COS(deg2rad($azimuth-$azimuthModul)));
         ($aoi > $limitAOI) ? $aoiKorr = $limitAOI : $aoiKorr = $aoi;
-        dump("Azimuth: $azimuth | Zenit: $zenitwinkel | AOI: $aoi");
+        #dump("Azimuth: $azimuth | Zenit: $zenitwinkel | AOI: $aoi");
         $dayAngel       = 6.283185*($tag-1)/365;
         $etr            = 1370*(1.00011+0.034221*COS($dayAngel)+0.00128*SIN($dayAngel)+0.000719*COS(2*$dayAngel)+0.000077*SIN(2*$dayAngel));
         ($zenitwinkel < 80) ? $am = (1/(COS(deg2rad($zenitwinkel))+0.15/(93.885-$zenitwinkel)**1.253)) : $am = 0;
         ($am > 0)           ? $kt = $ghi/(COS(deg2rad($zenitwinkel))*$etr) : $kt = 0.0;
-        dump("ETR: $etr | AM: $am | KT: $kt");
+        #dump("ETR: $etr | AM: $am | KT: $kt");
         $dniMod = 0.0;
         if ($kt>0) {
             if ($kt>=0.6) {
@@ -218,10 +218,10 @@ class WeatherFunctionsService
             }
             $dkn = $a+$b*EXP($c*$am);
             $knc = 0.886-0.122*$am+0.0121*($am)**2-0.000653*($am)**3+0.000014*($am)**4;
-            dump("a: $a | b: $b | c: $c | dkn: $dkn | knc: $knc");
+            #dump("a: $a | b: $b | c: $c | dkn: $dkn | knc: $knc");
             $dni = $etr*($knc-$dkn);
             $dniMod = $dni*COS($aoiKorr);
-            dump("DNI: $dni | DNImod: $dniMod");
+            #dump("DNI: $dni | DNImod: $dniMod");
         }
         $diffusMod = $ghi - $dniMod;
 
@@ -229,7 +229,7 @@ class WeatherFunctionsService
         $iam            = 1-0.05*((1/COS($aoi)-1));
         $gmod2          = $gmod1-$iam; // Modulstrahlung 2
         if ($gmod2 < 0) $gmod2 = 0; // Negative Werte machen keinen Sinn
-        dump("Stunde: $stunde Diffus: $diffusMod | Gmod1: $gmod1 | IAM: $iam | Gmod2: $gmod2 | GHI: $ghi");
+        #dump("Stunde: $stunde Diffus: $diffusMod | Gmod1: $gmod1 | IAM: $iam | Gmod2: $gmod2 | GHI: $ghi");
 
         return $gmod2;
     }
