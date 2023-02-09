1920b89f0cef8ffb2dd352ff5488e3684743bd17
Matthias <77692994+mr-g4n@users.noreply.github.com>
2023-02-09 09:44:19 +0100

    fix dc current inverter chart

--- src/Service/ExpectedService.php
+++ src/Service/ExpectedService.php
@@ -229,7 +229,7 @@ class ExpectedService
 
                         // degradation abziehen (degradation * Betriebsjahre).
                         $expVoltageDcHlp = $expVoltageDcHlp - ($expVoltageDcHlp / 100 * $modul->getModuleType()->getDegradation() * $betriebsJahre);
-                        if ($anlage->getAnlId() == '183') { // im Moment nur für REGebeng
+                        if ($anlage->getAnlId() == '183' || $anlage->getAnlId() == '175') { // im Moment nur für REGebeng und Perleberg
                             // Calculate DC power by current and voltage
                             $expPowerDcHlp = $expCurrentDcHlp * $expVoltageDcHlp / 4000;
                         } else {
