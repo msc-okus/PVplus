91842af982d7bc3e975c4fd2027cec0ebdda8676
JoseMorenoMartin <jm@green4net.com>
2023-05-10 14:15:43 +0200

    fixes with the user allowed plants in the tickets made

--- src/Repository/AnlagenRepository.php
+++ src/Repository/AnlagenRepository.php
@@ -294,6 +294,7 @@ class AnlagenRepository extends ServiceEntityRepository
 
     public function querBuilderFindAllActiveAndAllowed(): QueryBuilder
     {
+
         $qb = $this->createQueryBuilder('a')
             ->leftJoin('a.economicVarNames', 'varName')
             ->leftJoin('a.economicVarValues', 'ecoValu')
@@ -309,11 +310,19 @@ class AnlagenRepository extends ServiceEntityRepository
             /** @var User $user */
             $user = $this->security->getUser();
             $granted = $user->getGrantedArray();
-            $qb
-                ->andWhere("a.anlHidePlant = 'No'")
-                ->andWhere("a.anlView = 'Yes'")
-                ->andWhere("a.anlId IN (:granted)")
-                ->setParameter('granted', $granted);
+            if ($user->getAllPlants()) {
+                $qb
+                    ->andWhere('a.eignerId = :eigner')
+                    ->andWhere("a.anlHidePlant = 'No'")
+                    ->andWhere("a.anlView = 'Yes'")
+                    ->setParameter('eigner', $user->getEigners()[0]);
+            } else {
+                $qb
+                    ->andWhere("a.anlHidePlant = 'No'")
+                    ->andWhere("a.anlView = 'Yes'")
+                    ->andWhere("a.anlId IN (:granted)")
+                    ->setParameter('granted', $granted);
+            }
         }
         $qb
             ->orderBy('a.anlName', 'ASC') //a.eigner
