1bbbf6c6fc7cabe8718230644a06289b516e6dfc
JoseMorenoMartin <jm@green4net.com>
2023-06-28 11:57:04 +0200

    quickfixes in the ticket

--- src/Controller/DefaultJMController.php
+++ src/Controller/DefaultJMController.php
@@ -67,8 +67,8 @@ class DefaultJMController extends AbstractController
 
     #[Route(path: '/test/read', name: 'default_read')]
     public function testread(FunctionsService $fs, AnlagenRepository $ar, WeatherServiceNew $weather, AssetManagementService $am){
-        $anlage = $ar->findIdLike("57")[0];
-        $am->createAmReport($anlage, "11", "2022");
+        $anlage = $ar->findIdLike("207")[0];
+        dd($anlage->getMinIrrThreshold());
 
         return $this->render('base.html.twig');// this is suposed to never run so no problem
     }
