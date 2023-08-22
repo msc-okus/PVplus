f4fc92a3caa402a527846b3e07a720184f3d056c
gschuebert <gs@green4net.com>
2023-08-22 09:52:04 +0200

    remoove unused files

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,11 +1,7 @@
 <?php
 
 return [
-    'BASE_DATABASE_PASSWORD' => null,
-    'DATABASE_PASSWORD' => null,
-    'PLANT_DATABASE_PASSWORD' => null,
     'host' => null,
     'password_base' => null,
-    'password_data' => null,
     'password_plant' => null,
 ];
