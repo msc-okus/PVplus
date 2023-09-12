c66cc8b4eaed893a8d366759623983b285669bac
Matthias <77692994+mr-g4n@users.noreply.github.com>
2023-09-12 15:27:38 +0200

    adjust dummySollService

--- src/Service/DummySollService.php
+++ src/Service/DummySollService.php
@@ -8,6 +8,15 @@ class DummySollService
 {
     use G4NTrait;
 
+    public function __construct(
+        private $host,
+        private $userBase,
+        private $passwordBase,
+        private $userPlant,
+        private $passwordPlant,
+    )
+    {
+    }
     public function createDummySoll($from = null): string
     {
         $conn = self::getPdoConnection($this->host, $this->userPlant, $this->passwordPlant);
