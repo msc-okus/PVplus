ede7137c048a41ed1f90d5d5a0f88916f3d93900
gschuebert <gs@green4net.com>
2024-01-29 19:57:27 +0100

    save work today

--- assets/controllers/processmessengesshowmessage_controller.js
+++ assets/controllers/processmessengesshowmessage_controller.js
@@ -12,12 +12,14 @@ export default class extends Controller {
         var messageelem = document.getElementById('messageProzessReady');
         var messagetext = document.getElementById('messagetext');
         var pdfdownload = document.getElementById('pdfdownload');
+        var messagebutton = document.getElementById('far-fa-bell');
 
         //messageelem.style.display = 'block';
         this.fadeInElement(messageelem);
         messagetext.innerText = this.messageValue;
         pdfdownload.innerHTML = '<div>You can dowanload it here: <a class="hollow button tiny action-icon shadow" href="/reporting/pdf/'+this.prozessidValue+'" target="_blank"><span style="background-color:#ffffff" class="fa fa-file-pdf"></span></a></div>'
 
+        messagebutton.addAttr 
         window.setTimeout(() => {
             this.fadeOutElement(messageelem);
         }, 10000);
@@ -44,10 +46,14 @@ export default class extends Controller {
 
         $('#messagestabs div').addClass('fade');
         $('#messagestabs div').attr("style","display:none !important");
+        $('.messagebuttons ul li').removeClass('is-active');
+        $('.messagebuttons ul li').addClass('is-inactive');
 
         var tabtoshow = document.getElementById(value);
         $('#'+value).attr("style","display:block !important");
         $('#'+value).removeClass('fade');
+        $('.messagebuttons ul #li'+value).removeClass('is-inactive');
+        $('.messagebuttons ul #li'+value).addClass('is-active');
         this.fadeInElement(tabtoshow);
 
     }
