a12ca8762249252279bed307f5657c2e33cb6f98
steve <sd@green4net.com>
2024-02-12 16:09:25 +0100

    12.02.2024 PvpDataToDivisionGrouped

--- src/Command/PvpDataToDivisionGroupedCommand.php
+++ src/Command/PvpDataToDivisionGroupedCommand.php
@@ -6,16 +6,17 @@ use App\Service\PvpDivisionService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'PvpDataToDivision',
-    description: 'Übertragung von einzelnen DB',
+    name: 'PvpDataToDivisionGrouped',
+    description: 'Übertragung von allen DB',
 )]
-class PvpDataToDivisionCommand extends Command
+class PvpDataToDivisionGroupedCommand extends Command
 {
     public function __construct(private readonly PvpDivisionService $tableService)
     {
@@ -26,15 +27,13 @@ class PvpDataToDivisionCommand extends Command
     {
 
         $this
-            ->addOption('sm', null, InputOption::VALUE_REQUIRED, 'The initial month for data transfer' )
+            ->addOption('sm', null, InputOption::VALUE_REQUIRED, 'The initial month for data transfer')
             ->addOption('em', null, InputOption::VALUE_REQUIRED, 'The final month for data transfer')
             ->addOption('y', null, InputOption::VALUE_REQUIRED, 'The year for data transfer')
-            ->addOption('db', null, InputOption::VALUE_REQUIRED, 'The database name for data transfer')
         ;
     }
 
-    private function solo(ProgressBar $progressBar,int $year, int $month,string $dbname){
-
+    private function group(ProgressBar $progressBar,int $year, int $month){
 
         //It calculates the start date of the month and the end date of the month .
         $startDate = date('Y-m-01 00:00:00', strtotime("$year-$month-01"));
@@ -42,22 +41,24 @@ class PvpDataToDivisionCommand extends Command
 
         // Indicate the start of the progress
         $progressBar->start();
-        $this->tableService->transferDataOne($startDate, $endDate,$dbname);
+
+
+        $this->tableService->transferData($startDate, $endDate);
+
         // Advance the progress bar
         $progressBar->advance();
 
         // Finish the progress
         $progressBar->finish();
-
     }
+
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $startMonth = $input->getOption('sm');
         $endMonth = $input->getOption('em');
         $year = $input->getOption('y');
-        $dbname = $input->getOption('db');
-        $db='db__pv_dcist_'.$dbname;
+
 
 
         $progressBar = new ProgressBar($output);
@@ -65,9 +66,10 @@ class PvpDataToDivisionCommand extends Command
         // Set the number of steps in the progress
         $progressBar->setMaxSteps($endMonth - $startMonth + 1);
 
+
         for ($month = $startMonth; $month <= $endMonth; $month++) {
 
-           $this->solo($progressBar,$year,$month , $db);
+            $this->group($progressBar,$year,$month);
 
         }
 
@@ -77,5 +79,5 @@ class PvpDataToDivisionCommand extends Command
         return Command::SUCCESS;
     }
 
-    /* php bin/console PvpDataToDivision --sm 7 --em 7 --y 2023 --db CX217 */
+    /* php bin/console PvpDataToDivisionGrouped --sm 7 --em 7 --y 2023 */
 }
