00f64d48bc8897d2c358e446a3c2e6154094a764
gschuebert <gs@green4net.com>
2024-03-01 15:37:02 +0100

    save todays work

--- assets/controllers/select-inverters_controller.js
+++ assets/controllers/select-inverters_controller.js
@@ -6,7 +6,17 @@ import $ from 'jquery';
 
 export default class extends Controller {
     static targets =    ['splitAlert', 'modal', 'modalBody', 'splitModal', 'splitForm', 'switch', 'deactivable',
-        'anlage', 'saveButton'];
+        'anlage', 'saveButton', 'AlertFormat', 'AlertDates', 'formBegin', 'formEnd', 'splitButton',
+        'splitDeploy','AlertInverter', 'Callout', 'formCategory', 'AlertCategory', 'headerExclude',
+        'headerReplace', 'headerReplacePower', 'headerReplaceIrr', 'headerHour', 'headerEnergyValue',
+        'headerIrrValue', 'headerCorrection', 'headerEvaluation', 'headerAktDep1', 'headerAktDep2',
+        'headerAktDep3', 'formReplace', 'fieldSensor', 'fieldReplacePower', 'fieldReplaceIrr', 'fieldHour',
+        'fieldEnergyValue', 'fieldIrrValue', 'fieldCorrection', 'fieldEvaluation', 'fieldAktDep1', 'fieldAktDep2',
+        'fieldAktDep3', 'formReplaceIrr', 'inverterDiv', 'formHour', 'formBeginHidden', 'formEndHidden', 'formBeginDate',
+        'formEndDate', 'formReasonSelect', 'formReasonText', 'headerReason', 'fieldReason', 'formkpiStatus', 'headerFormKpi',
+        'headerPRMethod', 'fieldPRMethod', 'scope', 'reasonInput', 'sensorDiv', 'contactModal', 'modalContactBody', 'contactButton', 'modalContactCreateBody',
+        'contactModalCreate', 'modalTimelineBody', 'timelineModal'];
+
     static values = {
         formUrl: String,
     }
@@ -54,5 +64,71 @@ export default class extends Controller {
         }
     }
 
+    checkTrafo({ params: { first, last, trafo }}){
+        let body = $('#inverters');
+        let checked = $("#trafo" + trafo).prop('checked');
 
+        body.find('input:checkbox[class=js-checkbox]').each(function (){
+            if ($(this).prop('id').substring(2) >= first) {
+                if ($(this).prop('id').substring(2) <= last){
+                    if (checked) $(this).prop('checked', true);
+                    else $(this).prop('checked', false);
+                }
+            }
+        });
+
+        $(this.switchTarget).prop('checked', false)
+        this.saveCheck();
+    }
+
+    checkSelect(){
+        let body = $('#inverters');
+
+        $(this.switchTarget).prop('checked', false)
+
+    }
+
+
+
+    selectAll(){
+        let body = $('#inverters');
+
+        if ($(this.switchTarget).prop('checked')) {
+
+            body.find('input:checkbox[class=js-checkbox-trafo]').each(function () {
+                $(this).prop('checked', true);
+            });
+            body.find('input:checkbox[class=js-checkbox]').each(function () {
+                $(this).prop('checked', true);
+            });
+
+            inverterString = '*';
+            inverterNameString = '*';
+        } else {
+            body.find('input:checkbox[class=js-checkbox-trafo]').each(function () {
+                $(this).prop('checked', false);
+            });
+            body.find('input:checkbox[class=js-checkbox]').each(function () {
+                $(this).prop('checked', false);
+            });
+        }
+
+    }
+
+    saveInverters(){
+
+        let body = $('#inverters');
+        let target = $('#sviewhidden');
+        let chekedInverters = '';
+        body.find('input:checkbox[class=js-checkbox]').each(function () {
+
+            if($(this).prop('checked')){
+                chekedInverters = chekedInverters+','+$(this).attr('name');
+            }
+
+        });
+        chekedInverters = chekedInverters.slice(0, -1);
+        target.prop('value', chekedInverters);
+
+    }
 }
