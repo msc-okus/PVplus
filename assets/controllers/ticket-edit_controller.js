215b751bd59484f2fc03c51efcb62f88b412dc98
JoseMorenoMartin <jm@green4net.com>
2024-04-15 10:50:49 +0200

    final version

--- assets/controllers/ticket-edit_controller.js
+++ assets/controllers/ticket-edit_controller.js
@@ -87,13 +87,11 @@ export default class extends Controller {
     }
 
     beginPlusTime(){
+        let td = $(this.firstDateEndTarget);
         const valueBegin = $(this.formBeginTarget).prop('value');
-        const valueEnd = $(this.formEndTarget).prop('value');
+        const valueEnd = td.find('input')[0].value;
         let date = new Date(valueBegin);
         let endDate = new Date(valueEnd);
-
-        let td = $(this.firstDateEndTarget);
-        console.log(td.find('input')[0].value, $(this.formEndTarget).prop('value'));
 
         if ($(this.formHourTarget).prop('checked') == true){
             var addTime = 60;
@@ -213,8 +211,8 @@ export default class extends Controller {
 
     }
     endMinusTime(){
-
-        const valueBegin = $(this.formBeginTarget).prop('value');
+        let td = $(this.lastDateBeginTarget);
+        const valueBegin = td.find('input')[0].value;
         let beginDate = new Date(valueBegin);
         if ($(this.formHourTarget).prop('checked') == true){
             var subTime = 60;
