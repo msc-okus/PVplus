99860d32c4ebe78d32e766cd239d7f061be07588
Matthias <77692994+mr-g4n@users.noreply.github.com>
2024-05-15 15:33:18 +0200

    reactivate 'cabelLoss', add option to 'DC Group List' to update cabelLoss

--- src/Form/Groups/DcGroupsSFGUpdateFormType.php
+++ src/Form/Groups/DcGroupsSFGUpdateFormType.php
@@ -13,6 +13,13 @@ class DcGroupsSFGUpdateFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('cabelLoss', TextType::class,[
+                'label'=>false,
+                'attr'=>[
+                    'placeholder'=>'Cabel Loss',
+                ],
+                'empty_data'=>null
+            ])
             ->add('secureLoss', TextType::class,[
                 'label'=>false,
                 'attr'=>[
