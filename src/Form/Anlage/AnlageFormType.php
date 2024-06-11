352ca9cf387d20f4d8d308598389b4cec8f4c213
JoseMorenoMartin <jm@green4net.com>
2024-06-11 13:06:47 +0200

    interface changes applied to the MRO contact modal

--- src/Form/Anlage/AnlageFormType.php
+++ src/Form/Anlage/AnlageFormType.php
@@ -748,7 +748,7 @@ class AnlageFormType extends AbstractType
 
             ->add('ActivateTicketSystem', SwitchType::class, [
                 'label' => 'Activate ticket autogeneration',
-                'help' => '[ActivateTicketSystem]',
+                'help' => '[ActivateTicketSystem] ',
                 'attr' => ['data-plant-target' => 'activateTicket', 'data-action' => 'plant#activateTicket'],
                 'disabled' => !$isG4NUser,
             ])
