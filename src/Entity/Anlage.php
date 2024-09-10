9818ebd481b3923a6974cb9296e34ee60f094a71
Matthias <77692994+mr-g4n@users.noreply.github.com>
2024-09-10 11:18:44 +0200

    small css adjustmenst on footer, cleanup, prepare for new option to search alerts by company

--- src/Entity/Anlage.php
+++ src/Entity/Anlage.php
@@ -3525,7 +3525,7 @@ class Anlage implements \Stringable
      */
     public function hasPVSYST(): bool
     {
-        return (intval($this->kwPeakPvSyst) > 0 &&  $this->showPvSyst);
+        return (intval($this->kwPeakPvSyst) > 0 && $this->showPvSyst);
     }
 
     public function getPicture(): ?string
